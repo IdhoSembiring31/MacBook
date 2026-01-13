3c1e0837cc067385faba8eba27974a19d36ca0d9
idho dev <idhosembiring6@gmail.com>
2026-01-13 15:14:58 +0700

    Implement Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+import { useEffect } from 'react';
+function Hero() {
+    const videoRef = useRef();
+    useEffect(() => {
+      if(videoRef.current)videoRef.current.playbackRate = 2
+    
+     
+    }, [])
+    
+  return (
+    <section id='hero'>
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="title.png" alt="macBookTitle" />
+        </div>
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
+
+        <button>
+            Buy
+        </button>
+        <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  )
+}
+
+export default Hero
